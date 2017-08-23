09d41a851d779fcb1f0243981dbfd3bbf93fafd0
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-23 17:07:37 -0400

    home page and price list now working

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,12 +12,55 @@ namespace view_models.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Message"] = "Home Message.";
+            ViewData["Article-1-Heading"] = "Hire a Professional Handyman - Call 12345";
+            ViewData["Article-1-Body"] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.At eam doctus oportere, eam feugait delectus ne. Quo cu vulputate persecuti. Eum ut natum possim comprehensam, habeo dicta scaevola eu nec. Ea adhuc reformidans eam. Pri dolore epicuri eu, ne cum tantas fierent instructior. Pro ridens intellegam ut, sea at graecis scriptorem eloquentiam.";
+
+            ViewData["Article-2-Heading"] = "Furniture Assembly";
+            ViewData["Article-2-Body"] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. At eam doctus oportere, eam feugait delectus ne. Quo cu vulputate persecuti. Eum ut natum possim comprehensam, habeo dicta scaevola eu nec. Ea adhuc reformidans eam.";
+
+            ViewData["Article-3-Heading"] = "Expert Plumbers";
+            ViewData["Article-3-Body"] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. At eam doctus oportere, eam feugait delectus ne. Quo cu vulputate persecuti. Eum ut natum possim comprehensam, habeo dicta scaevola eu nec. Ea adhuc reformidans eam.";
+
+            ViewData["Article-4-Heading"] = "Interior / Exterior Painting";
+            ViewData["Article-4-Body"] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. At eam doctus oportere, eam feugait delectus ne. Quo cu vulputate persecuti. Eum ut natum possim comprehensam, habeo dicta scaevola eu nec. Ea adhuc reformidans eam.";
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Pricelist()
         {
-            ViewData["Message"] = "Your application description page.";
+            var prices = new List<PriceModel>();
+
+            var serviceOne = new PriceModel
+            {
+                Service = "Oil Change",
+                Info = "Changing your oil",
+                Price = "$20"
+            };
+
+            var serviceTwo = new PriceModel
+            {
+                Service = "Tune-up",
+                Info = "Tuning up your engine",
+                Price = "$30"
+            };
+
+            var serviceThree = new PriceModel
+            {
+                Service = "Mow Lawn",
+                Info = "Mowing your lawn",
+                Price = "$50"
+            };
+            prices.Add(serviceOne);
+            prices.Add(serviceTwo);
+            prices.Add(serviceThree);
+
+            return View(prices);
+        }
+
+        public IActionResult References()
+        {
+            ViewData["Message"] = "Look at all the people who like me:";
 
             return View();
         }
@@ -25,6 +68,13 @@ namespace view_models.Controllers
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
---
+++ Models/PriceModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace view_models.Models
+{
+    public class PriceModel
+    {
+        public string Service {get; set;}
+        public string Info {get; set;}
+        public string Price {get; set;}
+    }
+}
