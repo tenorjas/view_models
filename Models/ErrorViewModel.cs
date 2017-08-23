5e0c810f20c617b3b34b4ad4e6cf8b4bd3152643
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-23 15:13:55 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace view_models.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
